b8428d59e9d6220bbd335f0bbd5be7eac0f09445
ngocyt001 <ngocyt001@gmail.com>
2024-04-25 00:02:54 +0700

    set up logger for gRPC server and Midleware loggin for Gateway server

--- util/config.go
+++ util/config.go
@@ -10,6 +10,7 @@ import (
 // the application that we read from file or environment variables
 
 type Config struct {
+	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBDriver             string        `mapstructure:"DB_DRIVER"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
 	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
