8f16241a122a9ff72968844100a1bd36568f18da
ngocyt001 <ngocyt001@gmail.com>
2024-04-30 12:03:05 +0700

    setup send email to verify user

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	db "github.com/ngocyt001/simplebank/db/sqlc"
 	_ "github.com/ngocyt001/simplebank/doc/statik"
 	"github.com/ngocyt001/simplebank/gapi"
+	"github.com/ngocyt001/simplebank/mail"
 	"github.com/ngocyt001/simplebank/pb"
 	"github.com/ngocyt001/simplebank/util"
 	"github.com/ngocyt001/simplebank/worker"
@@ -52,7 +53,7 @@ func main() {
 	}
 
 	taskDistributor := worker.NewRedisTaskDistributor(redisOpt)
-	go runTaskProcessor(redisOpt, store)
+	go runTaskProcessor(config, redisOpt, store)
 	go runGatewayServer(config, store, taskDistributor)
 	runGrpcServer(config, store, taskDistributor)
 }
@@ -70,8 +71,9 @@ func runDBMigration(migrationURL string, dbSource string) {
 	log.Info().Msg("db migrated successfully")
 }
 
-func runTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) {
-	taskProcessor := worker.NewRedisTaskProcessor(redisOpt, store)
+func runTaskProcessor(config util.Config, redisOpt asynq.RedisClientOpt, store db.Store) {
+	mailer := mail.NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+	taskProcessor := worker.NewRedisTaskProcessor(redisOpt, store, mailer)
 	log.Info().Msg("start task processor")
 	err := taskProcessor.Start()
 	if err != nil {
--- worker/processor.go
+++ worker/processor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/hibiken/asynq"
 	db "github.com/ngocyt001/simplebank/db/sqlc"
+	"github.com/ngocyt001/simplebank/mail"
 	"github.com/rs/zerolog/log"
 )
 
@@ -22,9 +23,10 @@ type TaskProcessor interface {
 type RedisTaskProcessor struct {
 	server *asynq.Server
 	store  db.Store
+	mailer mail.EmailSender
 }
 
-func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store, mailer mail.EmailSender) TaskProcessor {
 	logger := NewLogger()
 	redis.SetLogger(logger)
 
@@ -46,6 +48,7 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 	return &RedisTaskProcessor{
 		server: server,
 		store:  store,
+		mailer: mailer,
 	}
 }
 
